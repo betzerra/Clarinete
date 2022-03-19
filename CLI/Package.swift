6086a50dc20238d68899526d09bfbd84d4dde6c9
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2022-03-19 01:03:14 -0300

    Added Host argument

--- CLI/Package.swift
+++ CLI/Package.swift
@@ -12,14 +12,19 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "Clarinete", path: "../"),
-        .package(url: "https://github.com/mtynior/ColorizeSwift.git", from: "1.5.0")
+        .package(url: "git@github.com:mtynior/ColorizeSwift.git", from: "1.5.0"),
+        .package(url: "git@github.com:apple/swift-argument-parser.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "ClarineteCLI",
-            dependencies: ["Clarinete", "ColorizeSwift"]
+            dependencies: [
+                "Clarinete",
+                "ColorizeSwift",
+                    .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
         ),
         .testTarget(
             name: "ClarineteCLITests",

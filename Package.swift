39c0d4a842f36e7d8228d922551dba3e04e11d8b
Ezequiel Alejandro Becerra <ezequiel.becerra@gmail.com>
2022-10-07 15:52:14 -0300

    Using a fixed Pluma version (#7)

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "git@github.com:betzerra/Pluma.git", branch: "main")
+        .package(url: "git@github.com:betzerra/Pluma.git", exact: .init(0, 1, 0))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
